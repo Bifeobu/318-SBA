b95db3ee7468b01ca6100344e3d279b14a80d9e1
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-06-02 20:09:41 -0400

    template with pug

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-const fs = require("fs");
+const path = require('path');
 const userRouter = require("./routes/users.js")
 
-app.set("views", "./views")
-app.set("view engine", "html")
+app.set("views", path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Express', message: 'Hello!'});
+});
+
+// app.engine("template", (filePath, options, callback) => {
+//     fs.readFile(filePath, (err, content) => )
+// })
+
+// app.set("views", "./views")
+// app.set("view engine", "template")
 
 app.get('/', (req, res) => {
     console.log("first")
