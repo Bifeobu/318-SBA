6da13f906599f25d35a5936779ebb19e0fd56f05
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-06-03 12:55:22 -0400

    created data for users data

---
+++ data/users.js
@@ -0,0 +1,75 @@
+// "users" data
+const users = [
+    {
+        id: 1,
+        name: "Harriot Dimelow",
+        gender: "Female",
+        job_title: "Tax Accountant",
+        email: "hdimelow0@ox.ac.uk"
+      }, 
+      {
+        id: 2,
+        name: "Brier Muggleston",
+        gender: "Female",
+        job_title: "Recruiter",
+        email: "bmuggleston1@businessinsider.com"
+      }, 
+      {
+        id: 3,
+        name: "Amabel Hydes",
+        gender: "Female",
+        job_title: "Senior Quality Engineer",
+        email: "ahydes2@vistaprint.com"
+      }, 
+      {
+        id: 4,
+        name: "Kane Leacock",
+        gender: "Male",
+        job_title: "Sales Representative",
+        email: "kleacock3@seattletimes.com"
+      }, 
+      {
+        id: 5,
+        name: "Reagan Daly",
+        gender: "Male",
+        job_title: "Environmental Specialist",
+        email: "rdaly4@mit.edu"
+      }, 
+      {
+        id: 6,
+        name: "Gavra Bezley",
+        gender: "Female",
+        job_title: "Sales Associate",
+        email: "gbezley5@illinois.edu"
+      }, 
+      {
+        id: 7,
+        name: "Sarena Rioch",
+        gender: "Female",
+        job_title: "Office Assistant III",
+        email: "srioch6@discovery.com"
+      }, 
+      {
+        id: 8,
+        name: "Marlo Gunningham",
+        gender: "Male",
+        job_title: "Engineer IV",
+        email: "mgunningham7@hp.com"
+      }, 
+      {
+        id: 9,
+        name: "Karel Gosalvez",
+        gender: "Male",
+        job_title: "Paralegal",
+        email: "kgosalvez8@google.pl"
+      }, 
+      {
+        id: 10,
+        name: "Roddy Bambrick",
+        gender: "Male",
+        job_title: "Cost Accountant",
+        email: "rbambrick9@shutterfly.com"
+      },
+];
+
+module.exports = users;
