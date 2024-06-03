757fd76fd39faf2519cc34b2aac96266c5a94ed1
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-06-03 13:13:36 -0400

    changed homepage content

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Express', message: 'Hello!' });
+    res.render('index', { title: 'Express Corp', message: 'Welcome to Express Corp!', content: 'This is the records site for the employees of Express Corp.' });
 });
 
 app.get('/about', (req, res) => {
