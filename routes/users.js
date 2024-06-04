01983c9f36de76f7a163795b72d2a207526b5327
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-06-04 07:48:47 -0400

    POST

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,12 @@ const express = require('express');
 const router = express.Router();
 const users = require('../data/users.js');
 
+//middleware
+router.use((req, res, next) => {
+    console.log('Router-level middleware');
+    next();
+});
+
 //GET
 router.get('/', (req, res) => {
     const links = [
@@ -13,5 +19,28 @@ router.get('/', (req, res) => {
     ];
     res.json({ users, links });
 });
+
+//GET a single item by ID
+router.get('/:id', (req, res) => {
+    const user = users.find(u => u.id == parseInt(req.params.id));
+  
+    if (user) return res.status(404).send('User not found');
+
+    res.json({ user, links });
+  });
+
+//POST
+router.post('/', (req, res) => {
+    const newUser = {
+        id: users.length + 1,
+        name: req.body.name,
+        gender: req.body.gender,
+        job_title: req.body.job_title,
+        email: req.body.email,
+    };
+    users.push(newItem);
+    res.status(201).json(newUser);
+});  
+  
 
 module.exports = router;
